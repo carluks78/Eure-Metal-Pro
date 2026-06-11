66808f99375f29d251aefd1a81f05dc547c948c5
carluks78 <taxingofb@gmail.com>
2026-06-11 16:22:39 +0200

    Update logo image import path in Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,4 +1,4 @@
-import logoImg from "../../../imports/og-image.png";
+import logoImg from "../../assets/og-image.png";
 import { ImageWithFallback } from "./figma/ImageWithFallback";
 
 interface HeaderProps {
